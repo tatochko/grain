4083f814f0b78c77d475e56156b983fcac99ebe6
vadim <vadim.tatochko98@mail.ru>
2020-11-11 23:53:38 +0300

    slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
 $(function(){
 
+    $('.header__main-slider').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        slideWidth: 1075,
+    });
 
 
-  
 });
