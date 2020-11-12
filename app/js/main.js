4733abd5fdd1f750065bce4ceba6a75f319a0df2
vadim <vadim.tatochko98@mail.ru>
2020-11-12 21:31:35 +0300

    fix arrow

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,11 @@
 $(function(){
 
     $('.header__main-slider').slick({
-        infinite: true,
+        infinite: false,
         speed: 300,
         slidesToShow: 1,
+        prevArrow: '<button type = "button" class = "slider-arrows arrow-prev"></ button>',
+        nextArrow: '<button type = "button" class = "slider-arrows arrow-next"></ button>',
         adaptiveHeight: true,
         slideWidth: 1075,
     });
