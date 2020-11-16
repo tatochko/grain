b3295989ebd32d6f7d43bfd35b0b6f2be4b6a9bb
vadim <vadim.tatochko98@mail.ru>
2020-11-16 23:51:42 +0300

    catalog slider

--- app/js/main.js
+++ app/js/main.js
@@ -10,5 +10,14 @@ $(function(){
         slideWidth: 1075,
     });
 
+    $('.catalog__slider').slick({
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        prevArrow: '<button type = "button" class = "slider-arrows arrow-prev"></ button>',
+        nextArrow: '<button type = "button" class = "slider-arrows arrow-next"></ button>',
+        adaptiveHeight: true,
+    });
+
 
 });
