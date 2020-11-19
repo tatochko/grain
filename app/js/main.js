81a14b83c7cf6e39ee3b9a76febd4dbcac3ae352
vadim <vadim.tatochko98@mail.ru>
2020-11-19 19:56:44 +0300

    upd 4,5 screen

--- app/js/main.js
+++ app/js/main.js
@@ -19,5 +19,14 @@ $(function(){
         adaptiveHeight: true,
     });
 
+    $('.why__slider').slick({
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        prevArrow: '<button type = "button" class = "slider-arrows arrow-prev"></ button>',
+        nextArrow: '<button type = "button" class = "slider-arrows arrow-next"></ button>',
+        adaptiveHeight: true,
+    });
+
 
 });
