f406ca35f1b0ec4313ff82279547f63547c29009
vadim <vadim.tatochko98@mail.ru>
2020-11-25 20:41:54 +0300

    burger

--- app/js/main.js
+++ app/js/main.js
@@ -36,5 +36,45 @@ $(function(){
         adaptiveHeight: true,
     });
 
+    ////////////////////////////////////////////////////////// burger
+    var Menu = {
+
+        el: {
+        ham: $('.header__btn'),
+        menuTop: $('.header__btn-top'),
+        menuMiddle: $('.header__btn-middle'),
+        menuBottom: $('.header__btn-bottom')
+        },
+        
+        init: function() {
+        Menu.bindUIactions();
+        },
+        
+        bindUIactions: function() {
+        Menu.el.ham
+            .on(
+                'click',
+            function(event) {
+            Menu.activateMenu(event);
+            event.preventDefault();
+            }
+        );
+        },
+        
+        activateMenu: function() {
+        Menu.el.menuTop.toggleClass('header__btn-top-click');
+        Menu.el.menuMiddle.toggleClass('header__btn-middle-click');
+        Menu.el.menuBottom.toggleClass('header__btn-bottom-click'); 
+        }
+    };
+    
+    Menu.init();
+
+
+    $('.header__btn-global').on('click', function() {
+        $('.menu ul').slideToggle();
+
+    });
+
 
 });
